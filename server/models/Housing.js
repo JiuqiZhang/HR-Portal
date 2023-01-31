bae9cd4c04264f52d226729491badd4b7edc9ea6
jujuzz <yzz9666@gmail.com>
2023-01-31 15:14:30 -0800

    update ref type to info

--- server/models/Housing.js
+++ server/models/Housing.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const schema = mongoose.Schema({
     address: { type:String, required: true },
-    roommates: [{ type: refType, ref: "Roommate" }],
+    roommates: [{ type: refType, ref: "Info" }],
 	reports: [{ type: refType, ref: "Report" }],
 })
 
--- server/models/Roommate.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const schema = mongoose.Schema({
-    firstName: { type:String, required: true },
-    lastName: { type:String, required: true },
-    phoneNum: { type:Number, required: true },
-})
-
-module.exports = mongoose.model("Roommate", schema)
