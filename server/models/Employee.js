9954b78f40fcc9b8fca5b2ab24dce1ffd816aab1
JiuqiZhang <jz3337@nyu.edu>
2023-01-30 22:30:46 -0500

    added HR schema, Emplyee schema, VisaStatus schema

--- server/models/Employee.js
+++ server/models/Employee.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose")
 
 const schema = mongoose.Schema({
-    name: {type:String, required: true},
+    firstName: {type:String, required: true},
+    lastName: {type:String, required: true},
 	email: {type:String, required: true, unique: true},
     password: {type:String, required: true},
     
-
 })
-
-module.exports = mongoose.model("User", schema)
+module.exports = mongoose.model("Employee", schema)
