3c3076f6a4f761e2cd9cfb50bfa0d7adaede2bdc
jujuzz <yzz9666@gmail.com>
2023-02-01 02:48:13 -0800

    updated housing schema

--- server/models/Housing.js
+++ server/models/Housing.js
@@ -4,6 +4,8 @@ const schema = mongoose.Schema({
     address: { type:String, required: true },
     roommates: [{ type: refType, ref: "Info" }],
 	reports: [{ type: refType, ref: "Report" }],
+    landlord: { type: refType, ref: "Landlord" },
+    facility: { type: refType, ref: "Facility" },
 })
 
 module.exports = mongoose.model("Housing", schema)
