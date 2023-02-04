4373d8d016106e9daa1201fdf9142abc0539c9ca
jujuzz <yzz9666@gmail.com>
2023-02-03 22:43:55 -0800

    updated view comment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import "./styles/App.css";
-import Login from "./pages/Login/Login";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Main from "./pages/Main";
+// import Login from "./pages/Login/Login";
+// import { Routes, Route, BrowserRouter } from "react-router-dom";
+// import Main from "./pages/Main";
+import EmployeeProfile from "./pages/HR/EmployeeProfile";
+
 
 function App() {
   // const loggedIn = false;
@@ -12,13 +14,14 @@ function App() {
 
        
       </header> */}
-      <BrowserRouter>
+      {/* <BrowserRouter>
           <Routes>
             
             <Route path="*" element={<Main/>} />
             <Route path="/login" element={<Login />} />
           </Routes>
-        </BrowserRouter>
+        </BrowserRouter> */}
+      <EmployeeProfile/>
     </div>
   );
 }
