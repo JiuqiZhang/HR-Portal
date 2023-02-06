3488a82b950781c207b5e971bdfafb3211c3ee10
jujuzz <yzz9666@gmail.com>
2023-02-06 10:13:35 -0800

    added address and roomates in UI

--- frontend/src/components/Report/ReportTable.js
+++ frontend/src/components/Report/ReportTable.js
@@ -7,6 +7,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 import ViewComment from "./ViewComment";
 import API from '../../api';
 
@@ -21,34 +22,42 @@ export default function ReportTable() {
   }, []);
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Title</TableCell>
-            <TableCell>Description</TableCell>
-            <TableCell>Creater</TableCell>
-            <TableCell>Timestamp</TableCell>
-            <TableCell>Status</TableCell>
-            <TableCell>View comments</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {report.map((report, index) => (
-            <TableRow
-              key={"row-" + index}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell>{report.title}</TableCell>
-              <TableCell>{report.description}</TableCell>
-              <TableCell>{report.creater}</TableCell>
-              <TableCell>{report.timestamp}</TableCell>
-              <TableCell>{report.status}</TableCell>
-              <TableCell><ViewComment reportId={report._id} creater={report.creater}/></TableCell>
+    <>
+      <Typography variant="h6" component="h6" style={{margin: 20}}>
+        Housing Address: 400 Sunshine Ave, CA
+      </Typography>
+      <Typography variant="h6" component="h6" style={{margin: 20}}>
+        Roomates: Ken K, Mark M
+      </Typography>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Title</TableCell>
+              <TableCell>Description</TableCell>
+              <TableCell>Creater</TableCell>
+              <TableCell>Timestamp</TableCell>
+              <TableCell>Status</TableCell>
+              <TableCell>View comments</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {report.map((report, index) => (
+              <TableRow
+                key={"row-" + index}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell>{report.title}</TableCell>
+                <TableCell>{report.description}</TableCell>
+                <TableCell>{report.creater}</TableCell>
+                <TableCell>{report.timestamp}</TableCell>
+                <TableCell>{report.status}</TableCell>
+                <TableCell><ViewComment reportId={report._id} creater={report.creater} /></TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
